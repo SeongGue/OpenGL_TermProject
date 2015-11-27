2a6946233230d075c32158be757f51ea6a5be4b2
SeongGue <game_son20@naver.com>
2015-11-28 01:16:33 +0900

    텀프 코드 실험 시작

---
+++ OpenGL_TermProject/main.cpp
@@ -0,0 +1,72 @@
+#include <GL/glut.h>
+#include <stdio.h>
+
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w, int h);
+void Timerfunction(int value);
+void Mouse(int button, int state, int x, int y);
+void keyboard(unsigned char key, int x, int y);
+void Mouse_Move(int x, int y);
+//-----------------------------------------------
+
+
+struct Point
+{
+	float x;
+	float y;
+	float z;
+	bool collison;
+};
+
+
+
+void main(int argc, char *argv[])
+{
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("Homework");
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(Reshape);
+	glutMouseFunc(Mouse);
+	glutMotionFunc(Mouse_Move);
+	glutKeyboardFunc(keyboard);
+	glutTimerFunc(40, Timerfunction, 1);
+	glutMainLoop();
+}
+
+GLvoid drawScene(GLvoid)
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // 설정된 색으로 젂체를 칠하기
+
+
+	glutSwapBuffers();
+}
+
+GLvoid Reshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+
+}
+
+void Mouse(int button, int state, int x, int y)
+{
+
+}
+
+void Mouse_Move(int x, int y)
+{
+
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+
+}
+
+void Timerfunction(int value)
+{
+	glutPostRedisplay();
+	glutTimerFunc(40, Timerfunction, 1);
+}
