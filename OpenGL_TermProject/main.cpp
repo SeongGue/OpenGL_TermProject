e85f857052c54376a553e392d836f70a9185217d
SeongGue <game_son20@naver.com>
2015-12-03 02:16:18 +0900

    다중뷰포트 구현

--- OpenGL_TermProject/main.cpp
+++ OpenGL_TermProject/main.cpp
@@ -4,6 +4,10 @@
 #include <time.h>
 
 #define PIE	3.14
+#define GROUND	-4900
+#define X_END	5000
+#define Y_END	5000
+#define Z_END	5000
 
 GLvoid drawScene(GLvoid);
 GLvoid Reshape(int w, int h);
@@ -12,9 +16,13 @@ void Mouse(int button, int state, int x, int y);
 void keyboard(unsigned char key, int x, int y);
 void Mouse_Move(int x, int y);
 //-----------------------------------------------
+void Cmera_change();
+
 
 float t = 0.1;
 float v = 0;
+
+enum { XY_SURFACE = 0, PERSPECTIVE = 2 };
 
 struct Point
 {
@@ -24,8 +32,15 @@ struct Point
 	bool collison;
 };
 
-float x = -400, y =-300, z = 0;
-float speed = 200;
+int camera_viewpoint = XY_SURFACE;
+int w1 = 0, h1 = 0;
+
+float x = -X_END, y = -Y_END + 1000 , z = 0; //1픽셀당 1m
+float speed = 1000; //초당 1000미터
+float second = 0;
+float xz = 0;
+float xy_angle = 0;
+float xz_angle = 0;
 
 void main(int argc, char *argv[])
 {
@@ -44,9 +59,17 @@ void main(int argc, char *argv[])
 
 GLvoid drawScene(GLvoid)
 {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // 설정된 색으로 젂체를 칠하기
+	Cmera_change();
 
+	glColor3f(1, 1, 1);
+	glBegin(GL_QUADS);//바닥
+	{
+		glVertex3f(-X_END, GROUND, -Z_END);
+		glVertex3f(-X_END, GROUND, Z_END);
+		glVertex3f(X_END, GROUND, Z_END);
+		glVertex3f(X_END, GROUND, -Z_END);
+	}
+	glEnd();
 	glPushMatrix();
 	{
 		glTranslatef(x, y, z);
@@ -54,13 +77,23 @@ GLvoid drawScene(GLvoid)
 		glutSolidCube(50);
 	}
 	glPopMatrix();
+
+	glPushMatrix();
+	{
+		glTranslatef(5000, GROUND, 0);
+		glColor3f(0, 0, 1);
+		glutSolidCube(1000);
+	}
+	glPopMatrix();
+
 	glutSwapBuffers();
 }
 
 GLvoid Reshape(int w, int h)
 {
-	glViewport(0, 0, w, h);
-	glOrtho(-400, 400, -300, 300, -300, 300);
+	w1 = w;
+	h1 = h;
+	//glEnable(GL_DEPTH_TEST);
 }
 
 void Mouse(int button, int state, int x, int y)
@@ -75,15 +108,85 @@ void Mouse_Move(int x, int y)
 
 void keyboard(unsigned char key, int x, int y)
 {
-
+	if (key == '1')
+	{
+		camera_viewpoint = XY_SURFACE;
+	}
+	if (key == '2')
+	{
+		camera_viewpoint = PERSPECTIVE;
+	}
 }
 
 void Timerfunction(int value)
 {
-	t = 0.1;
-	v += 9.8 * t;
-	x += speed*cos(PIE/4) * t;
-	y += speed*sin(PIE/4)*t - v/2;
+	t = 0.025;
+	second += t;
+	xy_angle = PIE / 6;
+	xz_angle = 0;
+	v += 200 * t;
+	xz = speed*cos(xy_angle);
+	x += xz*cos(xz_angle) * t;
+	y += speed*sin(xy_angle) * t - v / 2 * t;
+	z += xz*sin(xz_angle) * t;
+	printf("x = %.f, y = %.f, z = %.f, %.f \n", x, y, z, second);
 	glutPostRedisplay();
-	glutTimerFunc(40, Timerfunction, 1);
+	glutTimerFunc(10, Timerfunction, 1);
+}
+
+void Cmera_change()
+{
+	//glLoadIdentity();
+	//if (camera_viewpoint == XY_SURFACE)
+	//	glOrtho(-X_END, X_END, -Y_END, Y_END, -Z_END, Z_END);
+
+	//if (camera_viewpoint == PERSPECTIVE){
+	//	gluPerspective(60.0f, w1 / h1, 1.0, Z_END * 4);
+	//	gluLookAt(-X_END * 2, -Y_END / 2, 0.0,
+	//		0.0, -Y_END + 2000, 0.0,
+	//		1.0, 1.0, 0.0);
+	//}
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // 설정된 색으로 젂체를 칠하기
+	
+	glViewport(0, 0, 300, 300);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-X_END, X_END, -Y_END, Y_END, -Z_END, Z_END);
+	glMatrixMode(GL_MODELVIEW);
+
+
+	glColor3f(1, 1, 1);
+	glBegin(GL_QUADS);//바닥
+	{
+		glVertex3f(-X_END, GROUND, -Z_END);
+		glVertex3f(-X_END, GROUND, Z_END);
+		glVertex3f(X_END, GROUND, Z_END);
+		glVertex3f(X_END, GROUND, -Z_END);
+	}
+	glEnd();
+	glPushMatrix();
+	{
+		glTranslatef(x, y, z);
+		glColor3f(1, 0, 0);
+		glutSolidCube(50);
+	}
+	glPopMatrix();
+
+	glPushMatrix();
+	{
+		glTranslatef(5000, GROUND, 0);
+		glColor3f(0, 0, 1);
+		glutSolidCube(1000);
+	}
+	glPopMatrix();
+
+	glViewport(400, 0, 400, 300);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60.0f, w1 / h1, 1.0, Z_END * 4);
+	gluLookAt(-X_END * 2, -Y_END / 2, 0.0,
+		0.0, -Y_END + 2000, 0.0,
+		1.0, 1.0, 0.0);
+	glMatrixMode(GL_MODELVIEW);
 }
